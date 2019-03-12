028eb4e71427678ebbb83f1c70242443ad362412
Hanzala Arfat <hanzalaarfat@Hanzalas-MacBook-Pro.local>
2019-03-13 00:13:05 +0530

    OTP init

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,25 +1,131 @@
 <template>
-  <div class="container">
-    <form class="form-signin">
-      <h3 class="h3 mb-3 font-weight-normal align-center">Sign In</h3>
-      <label for="inputEmail" class="sr-only">Phone Number</label>
-      <input
-        type="number"
-        id="inputEmail"
-        class="form-control"
-        placeholder="Phone number"
-        required
-        autofocus
-      >
-      <button class="btn btn-lg btn-primary btn-block mt-3" type="submit">Proceed</button>
-      <p class="mt-5 mb-3 text-muted">&copy; 2018-2019</p>
-    </form>
+  <div class="login-page">
+    <div class="container">
+      <div class="login" v-if="!bol">
+        <form class="form-signin">
+          <h3 class="h3 mb-3 font-weight-normal align-center">Sign In</h3>
+          <label for="inputEmail" class="sr-only">Phone Number</label>
+          <input
+            type="number"
+            id="inputEmail"
+            class="form-control"
+            placeholder="Phone number"
+            :maxlength="13"
+            v-model="phone"
+            required
+            autofocus
+          >
+          <button
+            id="sign-in-button"
+            class="btn btn-lg btn-primary btn-block mt-3"
+            type="submit"
+            @click.prevent="sendOtp"
+          >Proceed</button>
+          <p class="mt-5 mb-3 text-muted">&copy; 2018-2019</p>
+        </form>
+      </div>
+
+      <div v-else>
+        <form class="form-otp">
+          <h3 class="h3 mb-3 font-weight-normal align-center">OTP page</h3>
+          <label for="otp" class="sr-only">OTP Number</label>
+          <input
+            type="number"
+            id="otp"
+            class="form-control"
+            placeholder="OTP number"
+            v-model="otp"
+            required
+            autofocus
+          >
+          <button class="btn btn-lg btn-primary btn-block mt-3" type="submit" @click="enterOtp">Done</button>
+          <p class="mt-5 mb-3 text-muted">&copy; 2018-2019</p>
+        </form>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import firebase from "firebase";
+import { send } from "q";
+
 export default {
-  name: "Login"
+  name: "Login",
+  data() {
+    return {
+      bol: false,
+      phone: "",
+      otp: ""
+    };
+  },
+  mounted() {
+    firebase.auth().useDeviceLanguage();
+    let self = this;
+    window.recaptchaVerifier = new firebase.auth.RecaptchaVerifier(
+      "sign-in-button",
+      {
+        size: "invisible",
+        callback: function(response) {
+          // reCAPTCHA solved, allow signInWithPhoneNumber.
+          console.log("recaptcha Successful");
+          self.sendOtp();
+        }
+      }
+    );
+    window.recaptchaVerifier.render().then(function(widgetId) {
+      window.recaptchaWidgetId = widgetId;
+    });
+  },
+  methods: {
+    sendOtp() {
+      this.bol = true;
+      let self = this;
+      var phoneNumber = this.phone;
+      var appVerifier = window.recaptchaVerifier;
+      if (phoneNumber.toString().length >= 10) {
+        phoneNumber.toString().length === 10
+          ? (phoneNumber = "+91" + phoneNumber.toString())
+          : (phoneNumber = phoneNumber.toString());
+        firebase
+          .auth()
+          .signInWithPhoneNumber(phoneNumber, appVerifier)
+          .then(function(confirmationResult) {
+            // SMS sent. Prompt user to type the code from the message, then sign the
+            // user in with confirmationResult.confirm(code).
+            window.confirmationResult = confirmationResult;
+          })
+          .catch(function(error) {
+            // Error; SMS not sent
+            // ...
+            console.log("OTP sending failed: " + error);
+            window.recaptchaVerifier.render().then(function(widgetId) {
+              grecaptcha.reset(widgetId);
+            });
+          });
+      } else {
+        console.log("Number not valid");
+      }
+    },
+    enterOtp() {
+      let enteredOtp = self.otp;
+      if (enteredOtp.toString().length === 6) {
+        window.confirmationResult
+          .confirm(enteredOtp)
+          .then(function(result) {
+            // User signed in successfully.
+            var user = result.user;
+            console.log(user);
+            // ...
+          })
+          .catch(function(error) {
+            // User couldn't sign in (bad verification code?)
+            // ...
+            console.log("Wrong Verfication Code");
+          });
+      }
+    }
+  }
 };
 </script>
 
